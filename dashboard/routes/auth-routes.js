06e3dcb22fe6c7156789e5825bfbc0ee6423e5ed
Dhvit <77879772+dhvitOP@users.noreply.github.com>
2021-10-23 11:06:04 +0530

    Update auth-routes.js

--- dashboard/routes/auth-routes.js
+++ dashboard/routes/auth-routes.js
@@ -4,12 +4,12 @@ const authClient = require('../modules/auth-client');
 const sessions = require('../modules/sessions');
 
 const router = express.Router();
-
+const { client } = require("../../xp.js");
 router.get('/invite', (req, res) =>
-  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=874338511464046622&permissions=0&redirect_uri=https%3A%2F%2Fdyno-clone.dhvitop.repl.co%2Fauth&scope=bot`));
+  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&redirect_uri=${encodeURIComponent(config.dashboardURL + "/auth")}&scope=bot`));
 
 router.get('/login', (req, res) =>
-  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=874338511464046622&redirect_uri=https%3A%2F%2Fdyno-clone.dhvitop.repl.co%2Fauth&response_type=code&scope=identify%20guilds`));
+  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&redirect_uri=${encodeURIComponent(config.dashboardURL + "/auth")}&response_type=code&scope=identify%20guilds`));
 
 router.get('/auth-guild', async (req, res) => {
   try {
