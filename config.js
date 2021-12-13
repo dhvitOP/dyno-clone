a9e5d48576301c660055d3eaa4ce0247860af739
RIFKI BOTX <88698204+RIFKIBOTZ@users.noreply.github.com>
2021-12-13 19:39:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     "registercommands" : false,
-     "token": "ODc0MzM4NTExNDY0MDQ2NjIy.YRFhMQ.HXL3mA8RQ6ljBNxXQQ8VKYmRN8k",
+     "token": "OTE2MzMzMjc1MTM0NDU1ODM4.Yaon3Q.SAJgJN8s16rlSAFDATq48Hz0Y4M",
     "imageapi": "81ec44aee6ccbb692dabd2eb0e1454b7f0c5ef1dd805a939a291127eb26b0f5af5bb4a94f095bceade9417c25292e746504e378f2d0f3aa2864c3d775c32e76a",
-    "ownerID": ["720632216236851260", "780029934163722280"],
+    "ownerID": ["590762566569951285"],
 "prefix": ".",
 "chat": {
         "url": "http://api.brainshop.ai/get?bid=155777&key=O0Rla6COZJ8XSGPJ&uid=[uid]&msg=[msg]",
@@ -16,7 +16,7 @@ module.exports = {
 "api": "AIzaSyCX8Af4p3kAJpjn4189RV2Z8hvMFAMbKoY",
 "youtubeAPI": "AIzaSyCX8Af4p3kAJpjn4189RV2Z8hvMFAMbKoY",
 mainprefix: ".",
-"owner": "G U D B O Y",
+"owner": "R I F K I W A N D I",
 "mongourl": "mongodb+srv://bot-list-lol:SzRpE6eXNegtLRvs@cluster0.jme3y.mongodb.net/nothingxd?retryWrites=true&w=majority",
 "secret": "TKDHwzyl2iieCPgq9mp4O5gttwqEOm1c",
  "dashboardURL": "https://dyno-clone.dhvitop.repl.co",
@@ -43,7 +43,7 @@ defaultleavemessage: "{user} Left, Invited by {inviter}",
     grole: "Giveaway Manager", //If the member doesn't have permission to handle messages he can still use the giveaways commands if he has the role configured right here
 
     auth: {
-        support: "XXX", //The link of your Discord server
+        support: "https://dsc.gg/vansmpnetwork", //The link of your Discord server
         dperms: "8" //The permissions that the bot asks on we want to add it on a Discord server (8 = moderator)
     },
 } 
